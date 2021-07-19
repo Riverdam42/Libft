df6634c932b9b960dc87cb8fc218c9d827cbfc5a
Kosuke Kawano <kkawano@student.42tokyo.jp>
2021-07-19 13:02:07 +0900

    part2 update

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: kkawano <kkawano@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/10 21:20:44 by kkawano           #+#    #+#             */
-/*   Updated: 2021/07/18 18:02:46 by kkawano          ###   ########.fr       */
+/*   Updated: 2021/07/19 12:09:44 by kkawano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,22 @@
 
 size_t ft_strlcpy (char *dst, const char *src, size_t size)
 {
-	char obj1 = (char *)dst;
-	const char obj2 = (char *)src;
+	char obj1[] = (char *)dst;
+	const char obj2[] = (char *)src;
 
-	while (size > size - 1)
+	size_t i;
+	size_t src_len;
+
+	src_len = 0;
+	while  ()
+	if (obj1 == 0)
+	return (src_len);
+	i = 0;
+	while (size <= size - 1)
 	{
-		obj1 = obj2;
-		obj1++;
-		obj2++;
-		if (obj1 == '\0')
-			*dst = '\0';
-		return (src - obj2 -1);
+		obj1[i] = obj2[i];
+		i++;
 	}
+	obj1[i] = '\0';
+	return (src_len -1);
 }
